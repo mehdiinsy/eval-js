2985fc742e0ca4623a6d9b83a03e815483726d68
mehdiinsy <126080136+mehdiinsy@users.noreply.github.com>
2023-03-21 16:50:31 +0100

    Initial commit

---
+++ eval.js
@@ -0,0 +1,26 @@
+// Mehdi
+
+// 1
+let age = 25 ;
+// 2
+let nom = "Jean" ;
+// 3
+let somme = 2+9 ;
+// 4
+let chaine = "test" ;
+chaine.toUpperCase() ;
+// 5
+let tableau = [1,2,3,4,5] ;
+tableau.length() ;
+// 6
+let objet = {"nom","age"} ;
+
+// 7
+let nombre = "5.3678" ;
+nombre.tofixed(2)
+// 8
+let date ;
+date.getmonth()
+// 9
+let tableau2 = ["srtjsryj","ethst","trhshs"] ;
+tableau2.join() ;
